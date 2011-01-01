dee17f8f789997c3125415f2e03292bca0c1eebe
Mizery De Aria <mizerydearia@gmail.com>
2011-01-01 07:48:24 -0600

    added index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	//error_reporting(-1); ini_set("display_errors", 1);
+	require_once "include.php";
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <style type="text/css">
+	html, body, form { height: 100%; width: 100%; }
+	body { font-family: monospace; }
+	a:link, a:visited { text-decoration: none; }
+	a:hover { text-decoration: underline; }
+  </style>
+  <title>Bitcoin Control Panel</title>
+ </head>
+ <body>
+  <h1>Bitcoin Control Panel</h1>
+  <h2><a href="accounts.php">Accounts</a></h2>
+  <h2><a href="methods.php">Methods</a></h2>
+  <h2><a href="transactions.php">Transactions</a></h2>
+ </body>
+</html>
