090666031e61d6c44f0c6cdb4d783e4d37e04ea9
Mizery De Aria <mizerydearia@gmail.com>
2011-01-02 12:48:10 -0600

    missed an update

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
-	$CONFIG["error_log_file"] = "../../btc/err";
+	$CONFIG["error_log_file"] = "../../../btc/err";
 	$CONFIG["error_message"] = "Sorry, b0rk3d!";
 
 
 	// username and password must be separately stored in these files
-	$CONFIG["bitcoin_user_file"] = "../../btc/user";
-	$CONFIG["bitcoin_pass_file"] = "../../btc/pass";
+	$CONFIG["bitcoin_user_file"] = "../../../btc/user";
+	$CONFIG["bitcoin_pass_file"] = "../../../btc/pass";
 ?>
